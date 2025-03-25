86096f47a7a5dba528652192eab92b7107c82a49
Tiago Sanches da Silva <95151771+TiagoSanches98@users.noreply.github.com>
2025-03-25 13:46:20 +0000

    Adicionar servidor web básico com rotas para a página inicial

---
+++ comments.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+// Create web server
+// create a web server
+// create a route for the home page 
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to the home page!');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page not found');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
